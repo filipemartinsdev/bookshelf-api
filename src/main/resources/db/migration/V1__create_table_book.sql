c8bc4baa8187d0e20969226c2d49068d0a0523fe
filipemartinsdev <dev.filipemartins@gmail.com>
2025-12-06 23:12:52 -0400

    feat(migration): create new migration for table reading_status

--- src/main/resources/db/migration/V1__create_table_book.sql
+++ src/main/resources/db/migration/V1__create_table_book.sql
@@ -5,7 +5,7 @@ CREATE TABLE book(
 	author VARCHAR(50) NOT NULL,
 	publisher VARCHAR(25) NOT NULL,
 	pages INT NOT NULL,
-	status INT  REFERENCES reading_status NOT NULL,
+	status INT REFERENCES reading_status NOT NULL,
 	rating FLOAT,
 	book_cover BYTEA NOT NULL
 );
---
+++ src/main/resources/db/migration/V1__create_table_reading_status.sql
@@ -0,0 +1,4 @@
+CREATE TABLE reading_status (
+    id INT PRIMARY KEY,
+    description VARCHAR(25) NOT NULL UNIQUE
+);
