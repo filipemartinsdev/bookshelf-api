49f356428db59592935edbe734dd6a3776b54419
filipemartinsdev <dev.filipemartins@gmail.com>
2025-12-13 11:54:11 -0400

    fix: database tables

--- src/main/resources/db/migration/V1__create_table_book.sql
+++ src/main/resources/db/migration/V1__create_table_book.sql
@@ -1,3 +1,15 @@
+--CREATE TABLE book(
+--	id SERIAL PRIMARY KEY,
+--	title VARCHAR(50) UNIQUE NOT NULL,
+--	subtitle VARCHAR(50),
+--	author VARCHAR(50) NOT NULL,
+--	publisher VARCHAR(25) NOT NULL,
+--	pages INT NOT NULL,
+--	status INT REFERENCES reading_status NOT NULL,
+--	rating FLOAT,
+--	book_cover BYTEA NOT NULL
+--);
+
 CREATE TABLE book(
 	id SERIAL PRIMARY KEY,
 	title VARCHAR(50) UNIQUE NOT NULL,
@@ -7,5 +19,5 @@ CREATE TABLE book(
 	pages INT NOT NULL,
 	status INT REFERENCES reading_status NOT NULL,
 	rating FLOAT,
-	book_cover BYTEA NOT NULL
+	book_cover BYTEA
 );
--- src/main/resources/db/migration/V1__create_table_reading_status.sql
+++ src/main/resources/db/migration/V1__create_table_reading_status.sql
@@ -1,4 +1,12 @@
+--CREATE TABLE reading_status (
+--    id INT PRIMARY KEY,
+--    description VARCHAR(25) NOT NULL UNIQUE
+--);
+
 CREATE TABLE reading_status (
     id INT PRIMARY KEY,
     description VARCHAR(25) NOT NULL UNIQUE
 );
+
+INSERT INTO reading_status(id, description)
+VALUES (0, 'unread'), (1, 'reading'), (2, 'read');
