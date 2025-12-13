3cd9d96703b07c7ebba48d082e9da1aea27ac3e9
filipemartinsdev <dev.filipemartins@gmail.com>
2025-12-13 12:58:21 -0400

    fix: change null check to Exception treatment

--- src/main/resources/db/migration/V1__create_table_reading_status.sql
+++ src/main/resources/db/migration/V1__create_table_reading_status.sql
@@ -1,8 +1,3 @@
---CREATE TABLE reading_status (
---    id INT PRIMARY KEY,
---    description VARCHAR(25) NOT NULL UNIQUE
---);
-
 CREATE TABLE reading_status (
     id INT PRIMARY KEY,
     description VARCHAR(25) NOT NULL UNIQUE
