cfdfc4bbaba83c556eb599b79f2072ed90b2c2e7
filipemartinsdev <dev.filipemartins@gmail.com>
2025-12-21 18:59:59 -0300

    fix: fix application.yaml

--- src/test/java/com/bookshelf/BookshelfApplicationTests.java
+++ src/test/java/com/bookshelf/BookshelfApplicationTests.java
@@ -3,11 +3,11 @@ package com.bookshelf;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class BookshelfApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class BookshelfApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
